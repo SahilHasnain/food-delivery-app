bb4561b1a0e88ccd58a828652cc861e15bf30711
MD SAHIL HASNAIN <mdsahil1631@gmail.com>
2025-07-25 18:28:32 +0530

    initial commit

---
+++ lib/appwrite.ts
@@ -0,0 +1,107 @@
+import {Account, Avatars, Client, Databases, ID, Query, Storage} from "react-native-appwrite";
+import {CreateUserParams, GetMenuParams, SignInParams} from "@/type";
+
+export const appwriteConfig = {
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT!,
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!,
+    platform: "com.sahil.fooddelivery",
+    databaseId: '68836a6f002e3915b961',
+    bucketId: '68837795001088e50ec9',
+    userCollectionId: '68836b22001362a55753',
+    categoriesCollectionId: '68836c4c00097bbb6396',
+    menuCollectionId: '68836f77002534d80af9',
+    customizationsCollectionId: '68836f77002534d80af9',
+    menuCustomizationsCollectionId: '6883733a002a49a99124'
+}
+
+export const client = new Client();
+
+client
+    .setEndpoint(appwriteConfig.endpoint)
+    .setProject(appwriteConfig.projectId)
+    .setPlatform(appwriteConfig.platform)
+
+export const account = new Account(client);
+export const databases = new Databases(client);
+export const storage = new Storage(client);
+const avatars = new Avatars(client);
+
+export const createUser = async ({ email, password, name }: CreateUserParams) => {
+    try {
+        const newAccount = await account.create(ID.unique(), email, password, name)
+        if(!newAccount) throw Error;
+
+        await signIn({ email, password });
+
+        const avatarUrl = avatars.getInitialsURL(name);
+
+        return await databases.createDocument(
+            appwriteConfig.databaseId,
+            appwriteConfig.userCollectionId,
+            ID.unique(),
+            { email, name, accountId: newAccount.$id, avatar: avatarUrl }
+        );
+    } catch (e) {
+        throw new Error(e as string);
+    }
+}
+
+export const signIn = async ({ email, password }: SignInParams) => {
+    try {
+        const session = await account.createEmailPasswordSession(email, password);
+    } catch (e) {
+        throw new Error(e as string);
+    }
+}
+
+export const getCurrentUser = async () => {
+    try {
+        const currentAccount = await account.get();
+        if(!currentAccount) throw Error;
+
+        const currentUser = await databases.listDocuments(
+            appwriteConfig.databaseId,
+            appwriteConfig.userCollectionId,
+            [Query.equal('accountId', currentAccount.$id)]
+        )
+
+        if(!currentUser) throw Error;
+
+        return currentUser.documents[0];
+    } catch (e) {
+        console.log(e);
+        throw new Error(e as string);
+    }
+}
+
+export const getMenu = async ({ category, query }: GetMenuParams) => {
+    try {
+        const queries: string[] = [];
+
+        if(category) queries.push(Query.equal('categories', category));
+        if(query) queries.push(Query.search('name', query));
+
+        const menus = await databases.listDocuments(
+            appwriteConfig.databaseId,
+            appwriteConfig.menuCollectionId,
+            queries,
+        )
+
+        return menus.documents;
+    } catch (e) {
+        throw new Error(e as string);
+    }
+}
+
+export const getCategories = async () => {
+    try {
+        const categories = await databases.listDocuments(
+            appwriteConfig.databaseId,
+            appwriteConfig.categoriesCollectionId,
+        )
+
+        return categories.documents;
+    } catch (e) {
+        throw new Error(e as string);
+    }
+}
